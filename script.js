e3bfdf4b58755783b65bdd8780fba404142ac26a
Thendo Khomunala <168510273+KhomunalaThendo@users.noreply.github.com>
2025-08-30 19:32:54 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const fullnames = document.getElementById('fullnames').value.trim();
+const email = document.getElementById('email').value.trim();
+const phone = document.getElementById('phone').value.trim();
+const message = document.getElementById('message').value;
